c3cb3579b0ef3fb9b0de04d4b67c6bc1d74130c0
Randall Dizon <randallbdizon@gmail.com>
2023-10-23 13:42:15 -0700

    Moved files around and added more code to the *.js file.

---
+++ randall-portfolio/randall-index.js
@@ -0,0 +1,34 @@
+function promptForSecretCode() {
+    var secretCode = prompt("Please enter the secret code:");
+    if (secretCode === "rstlne") { // Replace with your actual secret code
+        window.location.href = "./bc4150d023d3255136db671d61ac93f2/RSTLNE.html"; // Redirect to the secret page
+    } else {
+        alert("Incorrect secret code. Please try again.");
+        // Change the stylesheet dynamically
+        changeStylesheet("bc4150d023d3255136db671d61ac93f2/rstlne.css"); // Replace "incorrect.css" with the path to your stylesheet
+        invertColors();
+    }
+}
+
+function changeStylesheet(stylesheetPath) {
+    // Create a new link element for the new stylesheet
+    var newStylesheet = document.createElement("link");
+    newStylesheet.rel = "stylesheet";
+    newStylesheet.href = stylesheetPath;
+
+    // Find the existing stylesheet link element in the document head
+    var head = document.head;
+    var existingStylesheet = head.querySelector("link[rel='stylesheet']");
+
+    // Replace the existing stylesheet with the new one
+    if (existingStylesheet) {
+        head.replaceChild(newStylesheet, existingStylesheet);
+    } else {
+        head.appendChild(newStylesheet);
+    }
+}
+
+function invertColors() {
+    // Add a class to the body element to invert colors
+    document.body.classList.add("invert-colors");
+}
