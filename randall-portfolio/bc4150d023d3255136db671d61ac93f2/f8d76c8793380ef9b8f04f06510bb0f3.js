8ae93f9b5593051cbe17860c40f63f37106b8394
Randall Dizon <randallbdizon@gmail.com>
2023-10-24 01:00:39 -0700

    Made some tweaks to the alternative page.

--- randall-portfolio/bc4150d023d3255136db671d61ac93f2/f8d76c8793380ef9b8f04f06510bb0f3.js
+++ randall-portfolio/bc4150d023d3255136db671d61ac93f2/f8d76c8793380ef9b8f04f06510bb0f3.js
@@ -1,7 +1,7 @@
 function promptForSecretCode() {
     var secretCode = prompt("Please enter the secret code:");
     if (secretCode === "rstlne") { // Replace with your actual secret code
-        window.location.href = "./bc4150d023d3255136db671d61ac93f2/1d74fbc52e1db8c23d4dba04a0becc9f.html"; // Redirect to the secret page
+        window.location.href = "bc4150d023d3255136db671d61ac93f2/1d74fbc52e1db8c23d4dba04a0becc9f.html"; // Redirect to the secret page
     } else {
         alert("Incorrect secret code. Please try again.");
         // Change the stylesheet dynamically
